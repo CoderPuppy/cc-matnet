24020ad13996127d14444c421130953093619c31
Drew Young <drewyoung1@gmail.com>
2013-06-04 12:42:03 -0400

    Fix build script

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-robocopy %appdata%\.ccemu\saves\computer\1 %appdata%\.ccemu\saves\computer\2 * /MIR /MON:1
+@echo off
+robocopy %appdata%\.ccemu\saves\computer\1 %appdata%\.ccemu\saves\computer\2 * /MIR /NFL /nc /np /ns /NDL /NJS
